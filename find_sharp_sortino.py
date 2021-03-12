2c3ae97e4fdf63155f68c0fc1b9f3f1501e1a389
Profnastill <davidcom2007@yandex.ru>
2021-03-12 07:32:00 +0300

    Добавлена функция определения типа свечей

--- find_sharp_sortino.py
+++ find_sharp_sortino.py
@@ -3,6 +3,8 @@ import binance2 as bs
 import datetime
 import pandas as pd
 import math
+import xlwings as xw
+import datetime
 
 
 
@@ -45,18 +47,20 @@ def fun_sortino_(table_data):
 
 
 class Candle_class:
-    def __int__(self, table_data):
+    def __init__(self, table_data):
+
         self.candle_open = table_data["Open"]
         self.candle_hight = table_data["High"]
         self.candle_low = table_data["Low"]
         self.candle_close = table_data["Close"]
+        self.candle_model["свеча"]=table_data
 
     def candel_classificator(self):
         '''Определяет тип свечей'''
-        # if self.Candle_open/self.Т
         pogreshost = 0.5
         candel_scope = self.candle_low - self.candle_hight  # Размах свечи
         candel_size = math.fabs(self.candle_open - self.candle_close)  # Тело свечи
+        candel_relation=candel_size/self.candle_close#Отношение цены открытия к цене закрытия
         bottomShadow = min(self.candle_open, self.candle_close) - self.candle_low  # Нижняя тень  размер
         topShadow = self.candle_hight - max(self.candle_open, self.candle_close)  # Верхняя тень
 
@@ -64,36 +68,39 @@ class Candle_class:
             self.candle_type="Молот"
         elif topShadow >candel_size*2 and bottomShadow<0.1*candel_scope:
             self.candle_type="Такури"#Зонтик висильник
+        elif candel_size< 0.1*candel_scope and candel_relation <0.005:# вопросик по поводу диапазона
+            self.candle_type = "Додзи"
+        elif bottomShadow < topShadow * 4 and candel_relation<0.005:
+            self.candle_type="стрекоза"
+        elif (candel_scope-candel_size)/candel_scope<0.02:
+            self.candle_type="Белый больш свеча"
+        return self.candle_type
 
+def candel_classificator(table_data):
+    '''Определяет тип свечей'''
+    candle_open = table_data["Open"]
+    candle_hight = table_data["High"]
+    candle_low = table_data["Low"]
+    candle_close = table_data["Close"]
 
+    pogreshost = 0.5
+    candel_scope = candle_low - candle_hight  # Размах свечи
+    candel_size = math.fabs(candle_open - candle_close)  # Тело свечи
+    candel_relation=candel_size/candle_close#Отношение цены открытия к цене закрытия
+    bottomShadow = min(candle_open, candle_close) - candle_low  # Нижняя тень  размер
+    topShadow = candle_hight - max(candle_open, candle_close)  # Верхняя тень
 
-
-
-
-    def old(self):
-        if -pogreshost < (self.candle_open - self.candle_low) / self.candle_low * 100 < pogreshost \
-                and -pogreshost < (self.candle_close - self.candle_hight) / self.candle_hight < pogreshost \
-                and self.candle_close > self.candle_open:
-            self.candle_type = "больш.бел"
-        elif -pogreshost < (self.candle_open - self.candle_hight) / self.candle_hight * 100 < pogreshost \
-                and -pogreshost < (self.candle_close - self.candle_low) / self.candle_low < pogreshost \
-                and self.candle_close < self.candle_open:
-            self.candle_type = "больш.черн"
-        elif (self.candle_close - self.candle_hight) / self.candle_hight > 40 \
-                and self.candle_close > self.candle_open \
-                and (self.candle_open - self.candle_low) / self.candle_low < pogreshost:
-            self.candle_type = "Молот.бел"  # Требуется проверка насколько верны соотношения
-        elif (self.candle_open - self.candle_hight) / self.candle_hight > 40 \
-                and self.candle_close < self.candle_open and (self.candle_close - self.candle_low) / self.candle_low:
-            self.candle_type = "Молот.черн"
-
-    def candel_classificator(self):
-        '''Определяет тип свечей'''
-        None
-
-    def model_classificator(self):
-        '''классифицирует модели'''
-        None
+    if bottomShadow > candel_size*2 and topShadow <0.1* candel_scope:
+        candle_type="Молот"
+    elif topShadow >candel_size*2 and bottomShadow<0.1*candel_scope:
+        candle_type="Такури"#Зонтик висильник
+    elif candel_size< 0.1*candel_scope and candel_relation <0.005:# вопросик по поводу диапазона
+        candle_type = "Додзи"
+    elif bottomShadow < topShadow * 4 and candel_relation<0.005:
+        candle_type="стрекоза"
+    elif (candel_scope-candel_size)/candel_scope<0.02:
+        candle_type="Белый больш свеча"
+    return candle_type
 
 
 def take_data_candle(asset, daily_interval):
@@ -142,12 +149,51 @@ def take_data_candle(asset, daily_interval):
     sortino = fun_sortino_(table_data)  # Запуск функции пересчета Шарпа
 
     print(table_data)
-    print(f"Коэффициент {sharpa},Коэффициент Сортино {sortino}")
+    print(f"Коэффициент {sharpa},Коэффициент Сортино 0{sortino}")
     return pd.Series([sharpa, sortino])
 
 
+def ask_input():
+    try:
+        a=int(input("Введите по какой таблице искать по портфелю 1 или по рынку 0 "))
+    except:
+        "Ввели не числовые данные"
+        ask_input()
+    if a==1:
+        bs.table_base = bs.table
+        return bs.table_base
+    else:
+        bs.table_base = bs.table_base
+        return bs.table_base
+
+def insert_excel(table):
+    '''Функция для вставки в excel'''
+    a=int(input("Введите нужен ли импорт в excel Yes=1 "))
+    if a==1:
+        date=str(datetime.date.today())
+        xlbook=xw.Book(r"C:\Users\Давид\PycharmProjects\Binance\data_book.xlsx")
+        try:
+            xlsheet=xlbook.sheets.add(name=date)
+        except:
+            xlsheet=xlbook.sheets(date)
+
+        xlsheet.range("A1").options(index=False).value=table
+
+
+
+def test(data):
+    print("-"*10)
+    print (data)
+    a = Candle_class(data)#######
+    bs.table["Свечи"] = a.candel_classificator()
+    print(bs.table_base)###
+
+
+
 if __name__ == '__main__':
-    bs.table_base = bs.table_base
+    bs.table_base = ask_input()
+    bs.table_base=bs.table_base.loc[1:2]
+    #test(bs.table_base)
     # bs.table_base = bs.table  # Если надо найти по портфелю Шарпа включить эту строку.
     # print( bs.table)
     # take_data_candle(bs.table_base)
@@ -156,6 +202,9 @@ if __name__ == '__main__':
         lambda x: take_data_candle(x, 14))  # Инициализация функции поиска
     bs.table_base[["Sharp_60", "Sortino_60"]] = bs.table_base["asset"].apply(
         lambda x: take_data_candle(x, 60))  # Инициализация функции поиска
+
+
+
 
     bs.table_base = bs.table_base.dropna(subset=["Sortino_14"])
     print("-" * 10)
@@ -166,5 +215,6 @@ if __name__ == '__main__':
     bs.table_base.reset_index(drop=True, inplace=True)
     # print(bs.table_base)
     bs.table_base = bs.table_base[-20::1]
+    insert_excel(bs.table_base)# Функция для вставки в текущий excel
     print("-" * 20)
     print(bs.table_base)
