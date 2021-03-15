1f15b52c943cadee0c3d11882eb719fcbde97381
Profnastill <davidcom2007@yandex.ru>
2021-03-15 20:58:29 +0300

    Измененена функция Шарпа. Реализовано нахожждение через скользящее среднее

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-a=[1,2,3,4,5,6,7,8]
-print(a[-3::1])
+import pandas as pd
+
+df = pd.DataFrame({'ID':['1', '2', '3'], 'col_1': [0, 2, 3], 'col_2':[1, 4, 5]})
+mylist = ['a', 'b', 'c', 'd', 'e', 'f']
+
+print (df)
+def get_sublist(sta,end):
+    return mylist[sta:end+1]
+
+
+
+#df[["col_1","col_2"]]=df[["col_1","col_2"]].apply(lambda x: float(), axis=1)
+#df[["col_1","col_2"]]=df[["col_1","col_2"]].apply(float(), axis=1)
+df[["col_1","col_2"]]=df[["col_1","col_2"]].applymap(lambda x: float(x))
+
+max=list(df[["col_1","col_2"]].max())
+
+print(max)
+
+#print(df)
+
+
+print (abs(-4))
